e1d9e9492c68e964ac0c40c934dd7da643317a55
KumarAmitt <amitt2015@outlook.com>
2021-12-27 02:08:28 +0530

    Nest words in sub_goals

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,69 +1,48 @@
 class WordsController < ApplicationController
   before_action :set_word, only: %i[show edit update destroy]
+  before_action :find_sub_goal
 
-  # GET /words or /words.json
-  def index
-    @words = Word.all
-  end
-
-  # GET /words/1 or /words/1.json
-  def show; end
-
-  # GET /words/new
   def new
-    @word = Word.new
+    @word = @sub_goal.words.build
   end
 
-  # GET /words/1/edit
   def edit; end
 
-  # POST /words or /words.json
   def create
-    @word = Word.new(word_params)
+    @word = @sub_goal.words.build(word_params)
 
-    respond_to do |format|
-      if @word.save
-        format.html { redirect_to word_url(@word), notice: 'Word was successfully created.' }
-        format.json { render :show, status: :created, location: @word }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @word.errors, status: :unprocessable_entity }
-      end
+    if @word.save
+      redirect_to goals_path, notice: 'Word was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /words/1 or /words/1.json
   def update
-    respond_to do |format|
-      if @word.update(word_params)
-        format.html { redirect_to word_url(@word), notice: 'Word was successfully updated.' }
-        format.json { render :show, status: :ok, location: @word }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @word.errors, status: :unprocessable_entity }
-      end
+    if @word.update(word_params)
+      redirect_to goals_path, notice: 'Word was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /words/1 or /words/1.json
   def destroy
     @word.destroy
-
-    respond_to do |format|
-      format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to goals_path, notice: 'Word was successfully destroyed.'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  def find_sub_goal
+    @goal = Goal.find(params[:goal_id])
+    @sub_goal = SubGoal.find(params[:sub_goal_id])
+  end
+
   def set_word
     @word = Word.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def word_params
-    params.require(:word).permit(:name)
+    params.require(:word).permit(:name, :sub_goal_id)
   end
 end
